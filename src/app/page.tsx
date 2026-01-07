4ad7310523e97fd8bc671257d9767ffb74e93ed2
孙林 <1164700321@qq.com>
2026-01-07 14:57:20 +0800

    使用@apply复用css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
  * @Author: 孙林 1164700321@qq.com
  * @Date: 2026-01-04 15:35:22
  * @LastEditors: 孙林
- * @LastEditTime: 2026-01-06 18:10:58
+ * @LastEditTime: 2026-01-07 14:56:23
  * @Description: 
  */
 'use client'
@@ -37,11 +37,23 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="pt-18 text-xl">默认颜色</div>
+      <div className="pt-4 text-xl">默认颜色</div>
       <div className="text-primary text-xl dark:text-primary-black">测试自定义颜色</div>
 
-      <div className="pt-18 text-xl" onClick={() => goPage("/news")}>前往新闻列表</div>
-      <div className="pt-18 text-xl" onClick={() => goPage("/goods")}>前往商品列表</div>
+      <div className="pt-4 text-xl cursor-pointer" onClick={() => goPage("/news")}>前往新闻列表</div>
+      <div className="pt-4 text-xl cursor-pointer" onClick={() => goPage("/goods")}>前往商品列表</div>
+
+      <p className="text-xl mt-4 text-red-500">Tailwind通过使用 @apply、@layer 和 variants 等功能，你可以轻松创建可复用的样式，减少代码重复，提高开发效率和可维护性</p>
+      <ul >
+        <li className="groupClass">@apply：将常用的工具类组合成一个自定义类，减少重复代码。</li>
+        <li className="groupClass">@layer：定义样式层级，将不同的样式组合组织起来，便于管理。</li>
+        <li className="groupClass">variants：扩展工具类，使得样式可以根据状态（如响应式、悬停、聚焦等）变化。</li>
+      </ul>
+
+      <div className="pl-14">
+        <Image className="avatar" src="/test.jpg" alt="User 1" width={50} height={50} />
+        <button className="button">点击我</button>
+      </div>
     </div>
   );
 }
